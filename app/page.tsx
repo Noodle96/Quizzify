5ff0564f20ed87cd503ff9fe0219403d81b81f56
Noodle96 <jorgealfredomarino2@gmail.com>
2024-07-02 16:50:13 -0500

    Custom the button component

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return(
     <div>
-      <Button size="lg" variant="premium">Click me</Button>
+      <Button size="lg">Click me</Button>
     </div>
   );
 }
