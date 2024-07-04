15cba91a64be5aa8899688f51b87611860c4cab9
Noodle96 <jorgealfredomarino2@gmail.com>
2024-07-04 10:46:40 -0500

    Implement the footer section

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,56 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
     return (
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            footer
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button variant="ghost" size="lg" className="w-full">
+                    <Image
+                        src="/es.svg"
+                        alt="Spanish flag"
+                        width={40}
+                        height={32}
+                        className="mr-4 rounded-md"
+                    /> Español
+                </Button>
+                <Button variant="ghost" size="lg" className="w-full">
+                    <Image
+                        src="/fr.svg"
+                        alt="Frecnh flag"
+                        width={40}
+                        height={32}
+                        className="mr-4 rounded-md"
+                    /> Frances
+                </Button>
+                <Button variant="ghost" size="lg" className="w-full">
+                    <Image
+                        src="/hr.svg"
+                        alt="Croatia flag"
+                        width={40}
+                        height={32}
+                        className="mr-4 rounded-md"
+                    /> Croata
+                </Button>
+                <Button variant="ghost" size="lg" className="w-full">
+                    <Image
+                        src="/it.svg"
+                        alt="Italian flag"
+                        width={40}
+                        height={32}
+                        className="mr-4 rounded-md"
+                    /> Italiano
+                </Button>
+                <Button variant="ghost" size="lg" className="w-full">
+                    <Image
+                        src="/jp.svg"
+                        alt="Japan flag"
+                        width={40}
+                        height={32}
+                        className="mr-4 rounded-md"
+                    /> Japones
+                </Button>
+            </div>
         </footer>
     );
 }
