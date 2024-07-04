cb1ff7f91d6943d002790b3e85579406d4873d0d
Noodle96 <jorgealfredomarino2@gmail.com>
2024-07-04 10:17:38 -0500

    Implement the marketing page

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -12,8 +12,8 @@ import { Button } from "@/components/ui/button";
 export const Header = () => {
     return (
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            <div className="lg:max-w-screen-lg mx-auto flex  items-center \
-                            justify-between h-full">
+            <div className="lg:max-w-screen-lg mx-auto flex items-center \
+                            justify-between h-full pr-3">
                 <div className="pt-8 pl-4 pb-7 flex flex-row gap-x-3 ">
                     <Image
                         src="/mascot.svg"
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,62 @@
 import { Button } from "@/components/ui/button";
+import { Loader } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { ClerkLoaded,
+        ClerkLoading,
+        SignInButton,
+        SignUpButton,
+        SignedIn,
+        SignedOut
+} from "@clerk/nextjs";
 
 export default function Home() {
   return(
-    <div>
-      this is a marketing page
+    <div className="max-w-[988px] mx-auto flex flex-col flex-1 \ 
+                    w-full lg:flex-row items-center  \
+                    p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] \
+                      lg:h-[424px] mb-8 lg:mb-0">
+          <Image src="/hero.svg" fill alt="logo hero"/>
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Practica y aprende rapidamente e interactivamente con Quizzify.
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+
+            {/* significa que el usuario no esta autenticado */}
+            <SignedOut>
+              {/* afterSignInUrl = "/learn" afterSignUpUrl = "/learn" */}
+              {/* usuario crear una cuenta */}
+              <SignUpButton mode="modal" forceRedirectUrl="/learn">
+                <Button variant="secondary" size="lg" className="w-full">
+                  Comenzar
+                </Button>
+              </SignUpButton>
+              {/* usuario login(ya tiene una cuenta) */}
+              <SignInButton mode="modal" forceRedirectUrl="/learn">
+                <Button variant="primaryOutline" size="lg" className="w-full">
+                  Ya tengo una cuenta
+                </Button>
+              </SignInButton>
+            </SignedOut>
+
+            {/* significa que el usuario esta autenticado */}
+            <SignedIn>
+              {/* importante aschild props ya que estamos usando el componente Link
+              como hijo de Button */}
+              <Button variant="secondary" size="lg" className="w-full" asChild>
+                <Link href="/learn">Continuar aprendiendo</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   );
 }
