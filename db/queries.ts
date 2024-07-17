b82813af6a3a76efe4874ecec3f755e9b881b5e6
Noodle96 <jorgealfredomarino2@gmail.com>
2024-07-17 17:07:42 -0500

    Add getUserProgress

--- db/queries.ts
+++ db/queries.ts
@@ -1,5 +1,22 @@
 import {cache} from 'react';
+import { auth } from '@clerk/nextjs/server';
+import { eq } from 'drizzle-orm';
+
 import db from "./drizzle";
+import { userProgress } from './schema';
+
+export const getUserProgress = cache(async () => {
+    const {userId} = await auth();
+    if(!userId) return null;
+    
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with:{
+            activeCourse: true,
+        }
+    });
+    return data;
+});
 
 export const getCourses = cache(async () => {
     const data =  await db.query.courses.findMany();
