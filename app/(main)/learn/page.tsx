99c1a54e6e430121d80b3116839e28b510a05a6c
Noodle96 <jorgealfredomarino2@gmail.com>
2024-07-18 16:44:21 -0500

    edit LearnPage

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,23 @@
+import { getUserProgress } from "@/db/queries";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+    const getUserProgressData = getUserProgress();
+    const [
+        userProgress
+    ] = await Promise.all([
+        getUserProgressData
+    ]);
+    console.log("User: ", userProgress);
+    if(!userProgress || !userProgress.activeCourse){
+        console.log("!userProgress || !userProgress.activeCourse");
+        redirect("/courses");
+    }
+    
     return(
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
